fe221aebb814f7eab7d42f09f4e2b72fe1f7b3ee
Anas Aboradan <anas.aboradan@gmail.com>
2021-01-09 14:58:07 +0100

    first commit

--- screens/loginSystemScreens/logut.js
+++ screens/loginSystemScreens/logut.js
@@ -9,7 +9,7 @@ export default function Logut() {
   return (
       <View>
          <SafeAreaView style={{height:22}}></SafeAreaView>
-             <StatusBar backgroundColor="#08d4c4"/>
+                <StatusBar backgroundColor="#08d4c4"/>
  <Text>
  Logut and user Id is {global.UserID}
  </Text>
